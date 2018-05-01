a7ac3204d0d8e8bf54f6e3a6ddf32a19145e670d
Kilerd Chan <blove694@gmail.com>
2018-05-01 23:35:58 +0800

    docs: completed setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '0.2.5'
+__version__ = '0.0.1'
 
 here = path.abspath(path.dirname(__file__))
 
@@ -13,9 +13,9 @@ with open(path.join(here, 'readme.md'), encoding='utf-8') as f:
 setup(
     name='nougat_router',
     version=__version__,
-    description='Async web framework',
+    description='Router for nougat web framework supported restful api and parameters\' validation',
     long_description=long_description,
-    url='https://github.com/Kilerd/nougat',
+    url='https://github.com/Kilerd/nougat_router',
 
     author='Kilerd Chan',
     author_mail='blove694@gmail.com',
@@ -30,13 +30,12 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
 
-    keywords='web framework async',
+    keywords='web async router restful param',
 
     packages=find_packages(exclude=['docs', 'demo', 'tests*']),
     include_package_data=True,
     install_requires=[
         'nougat'
-
     ],
     python_requires='>=3.6',
 )
